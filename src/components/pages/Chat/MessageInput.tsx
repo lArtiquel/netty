0ccc52f6738fb52e6a2870154652be32e9c3c0e8
Artyom T. (Arti) <tsvirkoartem@gmail.com>
2022-12-04 02:52:09 +0300

    #14 Emoji support
    Signed-off-by: Artyom T. (Arti) <tsvirkoartem@gmail.com>

--- src/components/pages/Chat/MessageInput.tsx
+++ src/components/pages/Chat/MessageInput.tsx
@@ -12,11 +12,22 @@ import {
 import Firebase from 'firebase'
 import { useFirestore } from 'react-redux-firebase'
 import { ModalActions } from '../../../store/slice/ModalSlice'
+import EmojiPicker, {
+  EmojiClickData,
+  EmojiStyle,
+  SkinTonePickerLocation,
+  SkinTones,
+  SuggestionMode,
+  Theme
+} from 'emoji-picker-react'
+import EmojiEmotionsOutlinedIcon from '@material-ui/icons/EmojiEmotionsOutlined'
+import IconButton from '@material-ui/core/IconButton'
 
 const INITIAL_STATE = ''
 
 export default function MessageInput() {
   const [message, setMessage] = useState<string>(INITIAL_STATE)
+  const [isEmojiPickerOpen, setEmojiPickerOpen] = useState<boolean>(false)
 
   const firestore = useFirestore()
 
@@ -43,42 +54,74 @@ export default function MessageInput() {
       dispatch(ModalActions.openErrorModal(e))
     } finally {
       setMessage(INITIAL_STATE)
+      setEmojiPickerOpen(false)
     }
   }
 
+  function onEmojiClick(emojiData: EmojiClickData, event: MouseEvent) {
+    setMessage((message) => message + emojiData.emoji)
+  }
+
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      px={2}
-      py={1}
-      border={1}
-      borderColor="primary.light"
-      borderRadius={8}
-    >
-      <Box flexGrow={1} position="left" mr={3}>
-        <TextField
-          name="messageInput"
-          type="text"
-          autoFocus
-          fullWidth
-          margin="dense"
-          multiline
-          placeholder="Type something..."
-          maxRows={3}
-          value={message}
-          onChange={handleInputChange}
-        />
+    <Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        px={2}
+        py={1}
+        border={1}
+        borderColor="primary.light"
+        borderRadius={8}
+      >
+        <Box mr={1}>
+          <IconButton
+            aria-label="Emoji"
+            onClick={() => setEmojiPickerOpen(!isEmojiPickerOpen)}
+          >
+            <EmojiEmotionsOutlinedIcon />
+          </IconButton>
+        </Box>
+        <Box flexGrow={1} position="left" mr={3}>
+          <TextField
+            name="messageInput"
+            type="text"
+            autoFocus
+            fullWidth
+            margin="dense"
+            multiline
+            placeholder="Type something..."
+            maxRows={3}
+            value={message}
+            onChange={handleInputChange}
+            onClick={() => setEmojiPickerOpen(false)}
+          />
+        </Box>
+        <Box position="right">
+          <Button
+            disabled={!message.trim().length}
+            onClick={handleSend}
+            variant="outlined"
+          >
+            <SendOutlined />
+          </Button>
+        </Box>
       </Box>
-      <Box position="right">
-        <Button
-          disabled={!message.trim().length}
-          onClick={handleSend}
-          variant="outlined"
-        >
-          <SendOutlined />
-        </Button>
-      </Box>
+      {isEmojiPickerOpen && (
+        <Box mt={1}>
+          <EmojiPicker
+            onEmojiClick={onEmojiClick}
+            autoFocusSearch={false}
+            theme={Theme.DARK}
+            skinTonePickerLocation={SkinTonePickerLocation.PREVIEW}
+            suggestedEmojisMode={SuggestionMode.RECENT}
+            defaultSkinTone={SkinTones.NEUTRAL}
+            emojiStyle={EmojiStyle.GOOGLE}
+            height={350}
+            width="100%"
+            lazyLoadEmojis={true}
+          />
+        </Box>
+      )}
     </Box>
   )
 }
